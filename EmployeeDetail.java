e1b76a547bb768adf3d61d9d8eb20e577223ba88
Aravindrvr07 <aravindrvr07@gmail.com>
2024-01-16 09:54:33 +0530

    AAAAAA

---
+++ EmployeeDetail.java
@@ -0,0 +1,16 @@
+package org.test.tbm;
+
+public class EmployeeDetail {
+	
+	public void empId() {
+		
+		System.out.println("emp id 1111");
+	}
+	
+	public void empName() {
+		// TODO Auto-generated method stub
+System.out.println("emp name is ARAVIND");
+	}
+	
+
+}
